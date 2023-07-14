a6d568dd87cc5b5c8e73497f9de006356b737897
Wei Le Koh <weile.koh1286@gmail.com>
2023-07-15 02:56:03 +0800

    Added the Javascript for the sidebar interaction

---
+++ scripts/global.js
@@ -0,0 +1,26 @@
+function changeColourIn()
+{
+    closeButton = document.getElementById("closeButton");
+    closeButton.style.backgroundColor = "#c8b6a6";
+    closeButton.style.color = "#d9d9d9";
+}
+
+function changeColourOut()
+{
+    closeButton = document.getElementById("closeButton");
+    closeButton.style.color = "#000000";
+}
+
+function closeNav()
+{
+    document.getElementById("sidebarNav").style.width = "0";
+    
+}
+
+function openNav()
+{
+    // width of the sidebar navigation is set to a static px value //
+    // this is to prevent the content from shifting when the viewport is larger //
+    // the sidebar will only be used on mobile display, so a static value is fine //
+    document.getElementById("sidebarNav").style.width = "150px";
+}
